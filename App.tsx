61ebb56ec8cea740cde01216efa725fac3498d6a
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-09-07 20:29:00 -0300

    Create Header component and add to App

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import quotesData from "./data/quotes.json";
+import Header from "./components/header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
