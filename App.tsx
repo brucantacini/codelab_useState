ce60bc7be348b0402ffc65f4f775a9dc74537734
Bruno Cantacini <brunocantacini.ti@gmail.com>
2025-09-07 20:42:31 -0300

    Finishing project

--- App.tsx
+++ App.tsx
@@ -48,8 +48,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 16,
   },
 });
